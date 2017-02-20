bda9dc1fb4bc88d2153afc664be5f579a37041b3
sasbe <subedisagar52@gmail.com>
2017-02-20 16:01:55 +0530

    Working Version of Contact Manager
    Changes include:
    Pub/Sub is implemented.
    Loading progress bar is implemented.
    Contact list and details with save is implemented.

--- contact-manager/src/resources/index.ts
+++ contact-manager/src/resources/index.ts
@@ -2,4 +2,5 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   //config.globalResources([]);
+  config.globalResources(['./elements/loading-indicator']);
 }
